928e74fd9435f866ef5febc4618a1f50f815c8d9
Bilmakovchik <ruslanbilmakovskiy@gmail.com>
2018-01-24 16:32:10 +0200

    bug fix

--- main.js
+++ main.js
@@ -3,7 +3,6 @@ let imageDiv = document.getElementById('imageDiv');
 const image = document.getElementById('image');
 imageDiv.style.width = image.offsetWidth + 'px';
 
-
 function addBlock (e) {
     let imageParams = imageDiv.getBoundingClientRect();
     let div = document.createElement('DIV');
@@ -34,21 +33,27 @@ function addBlock (e) {
 document.getElementById('imageDiv').addEventListener('click', addBlock);
 
 function addDelButton(e) {
-    e.target.children[0].classList.toggle('del');
-    let imageParams = imageDiv.getBoundingClientRect();
-    let divParams = e.target.getBoundingClientRect();
+    if (!move) {
+        let del = e.target.children[0].getBoundingClientRect();
+        e.target.children[0].classList.toggle('del');
+        let imageParams = imageDiv.getBoundingClientRect();
+        let divParams = e.target.getBoundingClientRect();
 
-    checkOverflow(imageParams, divParams, e.target);
-    divParams = e.target.getBoundingClientRect();
-
-    if(e.target.children[0].classList.contains('del') // зменшити перевірку
-        && e.target.childNodes[0] === e.target.children[0]
-    ) {
-        console.dir(e.target);
-        e.target.style.left = parseFloat(e.target.style.left) + (imageParams.right - divParams.right) + 'px';
+        if(e.target.children[0].classList.contains('del')
+            && e.target.style.flexDirection === 'row-reverse')
+        {
+            console.dir(del.right);
+            console.dir(divParams.x);
+            console.dir(del.right - divParams.right);
+            e.target.style.left = parseFloat(e.target.style.left) + (divParams.right - del.right) / 2.0 + 5 + 'px';
+        }
+        checkOverflow(imageParams, divParams, e.target);
     }
 
+
+
     e.stopPropagation();
+    move = false;
 }
 
 function remove(e) {
@@ -58,6 +63,7 @@ function remove(e) {
 }
 
 function checkOverflow(parent, child, element) { // зробити чистою
+
     if (parent.top > child.top) {
         element.style.top = parseFloat(element.style.top) - (child.top - parent.top) + 'px';
     } if (parent.bottom < child.bottom) {
@@ -65,36 +71,45 @@ function checkOverflow(parent, child, element) { // зробити чистою
     } if (parent.left > child.left) {
         element.style.left = parseFloat(element.style.left) - (child.left - parent.left) + 'px';
     } if (parent.right < child.right) {
+
         element.style.left = parseFloat(element.style.left) - (child.right - parent.right) + 'px';
-        const delSpan = element.children[0];
-        element.removeChild(element.children[0]);
-        element.prepend(delSpan);
-    } if (parent.right > child.right) {
-        const delSpan = element.children[0];
-        element.removeChild(element.children[0]);
-        element.append(delSpan);
+        element.style.flexDirection = 'row-reverse';
+        // const delSpan = element.children[0];
+        // element.removeChild(element.children[0]);
+        // element.prepend(delSpan);
+    } if (parent.right >= child.right + 2) {
+        element.style.flexDirection = 'row';
+        // const delSpan = element.children[0];
+        // element.removeChild(element.children[0]);
+        // element.append(delSpan);
     }
 }
+
+let move = false;
 
 function take(e) {
     let divParams = this.getBoundingClientRect();
     let centerX = e.x - divParams.left;
     let centerY = e.y - divParams.top;
-    document.body.addEventListener('mousemove', (event) => {
-        let imageParams = imageDiv.getBoundingClientRect();
+    console.log('mouse down');
+    this.removeEventListener('click', addDelButton);
+    let imageParams = imageDiv.getBoundingClientRect();
+    document.body.onmousemove =((event) => {
+        move = true;
         divParams = this.getBoundingClientRect();
         if (event.clientX + (divParams.right - divParams.left) - centerX < imageParams.right
             && event.clientX - centerX > imageParams.left) {
             this.style.left = event.x - centerX - imageParams.left + 'px';
         }
         if (event.clientY + (divParams.bottom - divParams.top) - centerY < imageParams.bottom
-            && event.clientY -centerY > imageParams.top) {
+            && event.clientY - centerY > imageParams.top) {
             this.style.top = event.y - centerY - imageParams.top + 'px';
         }
         checkOverflow(imageParams, divParams, this);
-        e.stopPropagation();
-    })
+    });
+
+    this.addEventListener('mouseup', () => {
+        document.body.onmousemove = null;
+        this.addEventListener('click', addDelButton);
+    });
 }
-
-
-
